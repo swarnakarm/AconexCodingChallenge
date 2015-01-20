a47fc20400fc75ce20904d3373accbb973429888
swarnakarm <swarnakarmanish007@gmail.com>
2015-01-21 00:22:53 +0530

    Code Cover Report

--- 1800-FLOWER/test/com/aconex/test/TestRunner.java
+++ 1800-FLOWER/test/com/aconex/test/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.notification.Failure;
 public class TestRunner {
 
 	public static void main(String[] args) {
-	      Result result = JUnitCore.runClasses(TestAconex.class);
+	      Result result = JUnitCore.runClasses(TestNumberToAlphaConverter.class);
 	      for (Failure failure : result.getFailures()) {
 	         System.out.println(failure.toString());
 	      }
